b6c15f59510bd50c69975a8323cc69a05acbd089
LouCompute <Louiegarcia711@gmail.com>
2024-05-02 23:34:55 -0700

    Project 5: Sorting Things Out

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -1,23 +1,60 @@
 package com.example.sorting;
 
+import java.util.Arrays;
+import java.util.List;
 public class SortingUtility {
 
 
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
 
-        // TODO implement Gnome Sort here
+        int pos = 0;
+
+        while (pos < data.length) {
+            if (pos == 0 || data[pos].compareTo(data[pos - 1]) >= data[pos - 1].compareTo(data[pos]))
+            {
+                pos++;
+            }
+            else {
+                swap(data, pos, pos - 1);
+                pos--;
+            }
+        }
     }
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
 
-        // TODO implement Cocktail Shaker Sort here
+        boolean swapped;
+
+        do {
+            swapped = false;
+
+            for (int i = 0; i < data.length - 1; i++) {
+
+                if (data[i].compareTo(data[i + 1]) > data[i + 1].compareTo(data[i])) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+        } while(swapped);
     }
 
 
     public static <T extends Comparable<T>> void shellSort(T[] data) {
 
-        // TODO implement Shell Sort here
+        List<Integer> gaps = Arrays.asList(701, 301, 132, 57, 23, 10, 4, 1);
+
+        for (int gap : gaps) {
+            for (int i = gap; i < data.length; i++) {
+                T temp =data[i];
+                int j;
+
+                for (j = i; (j >= gap) && (data[j - gap].compareTo(temp) > 0); j-=gap) {
+                    data[j] = data[j - gap];
+                }
+                data[j] = temp;
+            }
+        }
     }
 
     private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
